18c8e9ea060a89f1d18c0a8f6cf54601b0707159
Riki-Zf <rikipatande81979@gmail.com>
2025-05-19 14:38:26 +0800

    configuration deploy

--- server/server.js
+++ server/server.js
@@ -8,7 +8,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5000", "https://kwitansi-pembayaran.vercel.app/"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/pembayaran", require("./routes/pembayaranRoutes"));
